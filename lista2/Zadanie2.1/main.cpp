13ba56ce8ce4239156997595451fa45f1a296287
Dominik Kowalczyk <282229@student.pwr.edu.pl>
2024-12-01 17:02:08 +0100

    quicksort modyfikacja poprawka + komentarze i main

--- lista2/Zadanie2.1/main.cpp
+++ lista2/Zadanie2.1/main.cpp
@@ -13,105 +13,132 @@ void RESETUJ() {                            //funkcja zerujaca po kazdym posorto
 }
 
 int PARTITION(float A[], int p, int r){     //int bo mamy zwrocic ostateczny indeks naszego pivota
-    float x = A[r];                         //x - pivot
-    int i = p - 1;                          //granica poczatkowa (ta z lewej strony), ktora przesuwamy jak pivot jak wiekszy
+    float x = A[r];
+    przypisania++;                          //x - pivot
+    int i = p - 1;
+    przypisania++;                          //granica poczatkowa (ta z lewej strony), ktora przesuwamy jak pivot jak wiekszy
 
-    for (int j = p; j <= r - 1; j++){       // pêtla iterujemy od p do r-1 (indkesy od 0 do r-1) przesuwajac sie w prawo
-        if (A[j] <= x){                     // warunek na to ze jesli nasza wartosc w j jest mniejsza od x to
-            i = i + 1;                      // granice przesuwamy o jedno w prawo
-            float temp = A[i];              // bierzemy tymczasowa zmienna, ktora przechowuje nam wartosc w indeksie i (w celu jej nie utracenia)
-            A[i] = A[j];                    // zastepujemy ja wartoscia na indeksie j
-            A[j] = temp;                    // przypisujemy wartoœæ z temp (czyli oryginaln¹ wartoœæ A[i]) do A[j]
+    for (int j = p; j <= r - 1; j++){       // pÃªtla iterujemy od p do r-1 (indkesy od 0 do r-1) przesuwajac sie w prawo
+        if (A[j] <= x){
+            porownania++;                                // warunek na to ze jesli nasza wartosc w j jest mniejsza od x to
+            i = i + 1;
+            przypisania++;                  // granice przesuwamy o jedno w prawo
+            float temp = A[i];
+            przypisania++;                  // bierzemy tymczasowa zmienna, ktora przechowuje nam wartosc w indeksie i (w celu jej nie utracenia)
+            A[i] = A[j];
+            przypisania++;                    // zastepujemy ja wartoscia na indeksie j
+            A[j] = temp;
+            przypisania++;                    // przypisujemy wartoÅ“Ã¦ z temp (czyli oryginalnÂ¹ wartoÅ“Ã¦ A[i]) do A[j]
         }
     }
-    float temp = A[i + 1];                  // jak juz nasza petla skonczy dzialac bierzemy wartosc i+1 do temp
-    A[i + 1] = A[r];                        // umiejscawaimy nasz pivot za nasza granica
-    A[r] = temp;                            // przypisujemy wartoœæ przechowan¹ w zmiennej temp (A[i + 1] przed zmiana)
-                                            //do komórki A[r], zwracamy indeks naszego podzialu, ktory nie bierze udzialu w quicksorcie
+    float temp = A[i + 1];
+    przypisania++;                          // jak juz nasza petla skonczy dzialac bierzemy wartosc i+1 do temp
+    A[i + 1] = A[r];
+    przypisania++;                          // umiejscawaimy nasz pivot za nasza granica
+    A[r] = temp;
+    przypisania++;                          // przypisujemy wartoÅ“Ã¦ przechowanÂ¹ w zmiennej temp (A[i + 1] przed zmiana)
+                                            //do komÃ³rki A[r], zwracamy indeks naszego podzialu, ktory nie bierze udzialu w quicksorcie
     return i + 1;
 }
 
 void QUICK_SORT(float A[], int p, int r){
-    if (p < r){                             //funkcja quicksort na bazie partition, znajduje dwa przedzialy ktore segregujemy
-        int  s = PARTITION(A,p,r);          // funkcjonowanie podobne jak merge sort
+    if (p < r){
+        porownania++;                                //funkcja quicksort na bazie partition, znajduje dwa przedzialy ktore segregujemy
+        int  s = PARTITION(A,p,r);
+        przypisania++;                              // funkcjonowanie podobne jak merge sort
         QUICK_SORT(A,p,s-1);
         QUICK_SORT(A,s+1,r);
     }
 }
 
-// Funkcja do partycjonowania tablicy na trzy czêœci przy u¿yciu dwóch pivota
+// Modyfikacja polegajaca na dzieleniu tablicy na trzy czesci przy uzyciu dwoch pivotow
 void PARTITION_MOD(float A[], int lewy, int prawy, int &p1Index, int &p2Index) {
-    // Wybór dwóch pivotów
     float p1 = A[lewy];
+    przypisania++;                  //wybieramy pivoty
     float p2 = A[prawy];
+    przypisania++;
 
-    // Zapewnienie, ¿e p1 < p2, zamiana miejscami jeœli to konieczne
+
     if (p1 > p2) {
+        porownania++;                            //sprawdzenie warunku, ktory jest wiekszy
         swap(A[lewy], A[prawy]);
+        przypisania +=2;                        //jesli konieczne zamienic miejscami
         p1 = A[lewy];
+        przypisania++;                           //przypisanie wartosci pivotom
         p2 = A[prawy];
+        przypisania++;
     }
 
-    // Inicjalizacja wskaŸników
-    int L = lewy + 1;
-    int S = lewy + 1;
+    // Inicjalizacja wskaÅºnikÃ³w
+    int L = lewy + 1;                               //miejsce gdzie bedziemy przenosci element mniejsze od p1
+    int S = lewy + 1;                               //aktualny przegladany indeks (w petli)
     int P = prawy - 1;
+    przypisania+=2;                              //miejsce gdzie bedziemy przenosic elementy wieksze od p2
 
-    // Proces partycjonowania
-    while (S <= P) {
+    while (S <= P) {                                //przechodzimy przez petle do momentu przejscia wszystkich indeksow
         if (A[S] < p1) {
+            porownania++;                           // jak wartosc jest mniejsza od pivota zamieniamy miejscami
             swap(A[S], A[L]);
+            przypisania++;                          //przesuwamy nasza granica o jedno miejsce w prawo
             L++;
+            przypisania++;
         } else if (A[S] >= p1 && A[S] <= p2) {
-            // Element nale¿y do czêœci II, nie wykonuj zamiany
-        } else { // A[K] > p2
-            while (A[P] > p2 && S < P)
+            porownania++;                               //jezeli A[S] ma wartosc pomiedzy pivotami to zostawiamy go bez zmian
+        } else { // A[S] > p2
+            porownania++;
+            while (A[P] > p2 && S < P)                               //szukamy od prawej strony elementu, ktÃ³ry nie jest wiÄ™kszy od p2
                 P--;
+                przypisania++;                               //p przesuwamy w lewa strone
             swap(A[S], A[P]);
+            przypisania+=2;                       //zamieniamy miejscami A[S] (wiekszy od p2) z A[P] (potencjalnie <=p2)
             P--;
+            przypisania++;                                    //zmniejszamy p bo ilosc elementow rozpatrywanych sie zmniejszyla
             if (A[S] < p1) {
+                porownania++;                       //zmniejszamy indeks bo ilosc elementow do posortowania sie zmniejszyla
                 swap(A[S], A[L]);
+                przypisania+=2;                     //jesli wartosc A[S] jest mniejsza od p1 to zamieniamy miejscami
                 L++;
+                przypisania++;                                //przesuwamy L (granice lewa) w prawo
             }
         }
         S++;
+        przypisania++;                                        //zwieskzmay indeks S aby przejsc do nastepenego elementu
     }
 
-    // Umieszczanie pivotów na w³aœciwych pozycjach
-    L--; P++;
-    swap(A[lewy], A[L]);
+    L--;                            // cofamy sie o jeden w lewo aby L wskazywalo ostatni element w strefie < p1
+    P++;                            // idziemy o jeden w prawo aby P wskazywalo pierwsze miejsce w strefie > p2
+    swap(A[lewy], A[L]);            //zamiana oryginalnego pivota z A[L]
     swap(A[prawy], A[P]);
+    przypisania+=6;                 //zamiana oryginalnego powiota z A[P]
 
-    // Aktualizacja indeksów pivotów
-    p1Index = L;
+    p1Index = L;                    //ustawiamy indeksy pivotow p1 i p2 do wlasciwego quicksorta (podzial na 3 obszary)
     p2Index = P;
+    przypisania+=2;
 }
 
-// Rekurencyjna funkcja wykonuj¹ca zmodyfikowany quicksort
 void QUICK_SORT_MOD(float A[], int lewy, int prawy) {
     if (lewy < prawy) {
-        // Partycjonowanie tablicy na trzy czêœci
+        porownania++;
         int p1Index, p2Index;
+        przypisania++;              //partition dzialanie
         PARTITION_MOD(A, lewy, prawy, p1Index, p2Index);
-
-        // Rekurencyjne sortowanie trzech czêœci
-        QUICK_SORT_MOD(A, lewy, p1Index - 1);   // Czêœæ I
-        QUICK_SORT_MOD(A, p1Index + 1, p2Index - 1); // Czêœæ II
-        QUICK_SORT_MOD(A, p2Index + 1, prawy);  // Czêœæ III
+        QUICK_SORT_MOD(A, lewy, p1Index - 1);   // Czesc I
+        QUICK_SORT_MOD(A, p1Index + 1, p2Index - 1); // Czesc II
+        QUICK_SORT_MOD(A, p2Index + 1, prawy);  // Czesc III
     }
 }
 
 void GENERATOR_TABLIC(float A[], int n) {
-    srand(time(0));                             //inicjalizacja generatora losowego
-    for (int i = 0; i < n; i++) {               //po kolei generujemy az do n
-        A[i] = static_cast<float>(rand()) / RAND_MAX * 50000.00;        //static_cast<float> - przekszta³camy na floata
-                                                    //liczbê ca³kowit¹ generowan¹ przez rand() / RAND_MAX
-    }                                               //dostajemy liczbe od 0 do 1, co mno¿ymy przez 50000
+    srand(time(0));                                                     //inicjalizacja generatora losowego
+    for (int i = 0; i < n; i++) {                                       //po kolei generujemy az do n
+        A[i] = static_cast<float>(rand()) / RAND_MAX * 50000.00;        //static_cast<float> - przeksztalcamy na floata
+                                                                        //liczbe calkowita generowana przez rand() / RAND_MAX
+    }                                                                   //dostajemy liczbe od 0 do 1, co mnoÂ¿ymy przez 50000
 }
 void WYPISZ_TABLICE(float* A, int n) {
     for (int i = 0; i < n; i++) {
-        cout << fixed << setprecision(3) << A[i] << " ";       //ustawioamy ze wyswietlane liczby w tablicy beda ze sta³¹
-    }                                                    //liczb¹ po przecinku (fixed) , 2 (setprecision)
+        cout << fixed << setprecision(3) << A[i] << " ";       //ustawiamy ze wyswietlane liczby w tablicy beda ze stale
+    }                                                           //liczbe po przecinku (fixed) , 2 (setprecision)
     cout << endl;
 }
 void WYPISZ_WYNIK(float A[], int n) {
@@ -121,20 +148,20 @@ void WYPISZ_WYNIK(float A[], int n) {
 }
 
 int main() {
-    int sizes[] = {10, 100, 1000, 10000, 50000}; // wielkosci tablic do testu
+    int sizes[] = {100000, 200000, 300000, 400000, 500000}; // wielkosci tablic do testu
     for (int i = 0; i < 5; i++) {
         int n = sizes[i];
         float* A = new float[n]; // dynamiczne przechowywanie tablicy A o dlugosci n
-        float* B = new float[n]; // dodatkowa tablica B do zapamiêtania oryginalnej kolejnoœci
+        float* B = new float[n]; // dodatkowa tablica B do zapamietania oryginalnej kolejnosci
 
         GENERATOR_TABLIC(A, n); // generujemy losowe liczby
 
-        // Kopiujemy wygenerowan¹ tablicê A do B, aby zapamiêtaæ pocz¹tkow¹ kolejnoœæ
+        // Kopiujemy wygenerowana tablice A do B, aby zapamietac poczatkowa kolejnosc
         for (int j = 0; j < n; j++) {
             B[j] = A[j];
         }
 
-        // Wyœwietlanie oryginalnej zawartoœci tablic A i B (dla rozmiaru 10)
+        // Wyswietlenie oryginalnej zawartosci tablic A i B (dla rozmiaru 10)
         if (n == 10) {
             cout << "Oryginalna tablica A: ";
             WYPISZ_TABLICE(A, n);
@@ -152,20 +179,20 @@ int main() {
         cout << "Czas trwania: " << elapsed_time << " ms" << endl;
         WYPISZ_WYNIK(A, n);
 
-        // Wyœwietlanie posortowanej tablicy A po normalnym sortowaniu (dla rozmiaru 10)
+        // Wyswietlanie posortowanej tablicy A po normalnym sortowaniu (dla rozmiaru 10)
         if (n == 10) {
             cout << "Posortowana tablica A po QUICK_SORT: ";
             WYPISZ_TABLICE(A, n);
         }
 
-        // Przywracamy oryginaln¹ kolejnoœæ liczb z B do A
+        // Przywracamy oryginalna kolejnosc liczb z B do A
         for (int j = 0; j < n; j++) {
             A[j] = B[j];
         }
 
-        // Wyœwietlanie przywróconej tablicy A (dla rozmiaru 10)
+        // Wyswietlanie przywrÃ³conej tablicy A (dla rozmiaru 10)
         if (n == 10) {
-            cout << "Tablica A przywrócona z B: ";
+            cout << "Tablica A przywrÃ³cona z B: ";
             WYPISZ_TABLICE(A, n);
         }
 
@@ -179,7 +206,7 @@ int main() {
         cout << "Czas trwania: " << elapsed_time << " ms" << endl;
         WYPISZ_WYNIK(A, n);
 
-        // Wyœwietlanie posortowanej tablicy A po zmodyfikowanym sortowaniu (dla rozmiaru 10)
+        // Wyswietlanie posortowanej tablicy A po zmodyfikowanym sortowaniu (dla rozmiaru 10)
         if (n == 10) {
             cout << "Posortowana tablica A po QUICK_SORT_MOD: ";
             WYPISZ_TABLICE(A, n);
